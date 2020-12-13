1623ea09eeae74b5e43f495fa7f38f1515a25746
Metalblueberry <metalblueberry@gmail.com>
2020-12-13 15:56:12 +0100

    add config

---
+++ pkg/chkb/config.go
@@ -0,0 +1,249 @@
+/*
+Copyright © 2020 Víctor Pérez @MetalBlueberry
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package chkb
+
+import (
+	"fmt"
+	"io"
+	"time"
+
+	"gopkg.in/yaml.v3"
+	// log "github.com/sirupsen/logrus"
+)
+
+const (
+	// DefaultTapDelay is the default setting for TapDelay
+	DefaultTapDelay = 200 * time.Millisecond
+)
+
+// Config is the configuration file structure
+type Config struct {
+	TapDelay int       `yaml:"tapDelay,omitempty"`
+	Layers   LayerBook `yaml:"layers"`
+}
+
+// Save writes the configuration content to a io.Writer
+func (b *Config) Save(w io.Writer) error {
+	encoder := yaml.NewEncoder(w)
+	return encoder.Encode(b)
+}
+
+// Load reads the configuration from a io.Reader
+func (b *Config) Load(r io.Reader) error {
+	encoder := yaml.NewDecoder(r)
+	return encoder.Decode(b)
+}
+
+// GetTapDelay returns the configured delay or the default if it is empty
+func (b *Config) GetTapDelay() time.Duration {
+	if b.TapDelay != 0 {
+		return time.Duration(b.TapDelay) * time.Millisecond
+	}
+	return DefaultTapDelay
+}
+
+// LayerBook contains all the layer configurations
+type LayerBook map[string]*Layer
+
+// Layers is a list of Layer
+type Layers []*Layer
+
+// Layer represents a behaviour of the keyboard that can be applied on demand
+type Layer struct {
+	OnMiss []MapEvent `yaml:"onMiss,omitempty"`
+	KeyMap KeyMap     `yaml:"keyMap,omitempty"`
+}
+
+// KeyMapActions contains the events mapped to an action
+type KeyMapActions map[KeyActions][]MapEvent
+
+func (km KeyMapActions) hasSpecialMaps() bool {
+	for k := range km {
+		switch k {
+		case KeyActionUp, KeyActionDown, KeyActionNil, KeyActionMap:
+			continue
+		default:
+			return true
+		}
+	}
+	return false
+}
+
+// KeyMap contains the keys with KeyMapActions
+type KeyMap map[KeyCode]KeyMapActions
+
+// StringMap generates the equivalent map with types removed, used for serialization
+func (km *KeyMap) StringMap() map[string]map[string][]MapEvent {
+	tmp := make(map[string]map[string][]MapEvent)
+	for keyCode, actionMap := range *km {
+		tmp[keyCode.String()] = make(map[string][]MapEvent)
+		for action, mapEvent := range actionMap {
+			tmp[keyCode.String()][action.String()] = mapEvent
+		}
+	}
+	return tmp
+}
+
+// FromStringMap reverses the action from StringMap
+func (km *KeyMap) FromStringMap(source map[string]map[string][]MapEvent) error {
+	if (*km) == nil {
+		(*km) = map[KeyCode]KeyMapActions{}
+	}
+	for keyCodeString, actionMap := range source {
+		keyCode, err := ParseKeyCode(keyCodeString)
+		if err != nil {
+			return err
+		}
+		(*km)[keyCode] = make(map[KeyActions][]MapEvent)
+
+		for actionString, mapEvent := range actionMap {
+			action, err := ParseKeyAction(actionString)
+			if err != nil {
+				return err
+			}
+			(*km)[keyCode][action] = mapEvent
+		}
+	}
+	return nil
+}
+
+// UnmarshalYAML reads yaml configuration
+func (km *KeyMap) UnmarshalYAML(value *yaml.Node) error {
+	tmp := make(map[string]map[string][]MapEvent)
+	err := value.Decode(&tmp)
+	if err != nil {
+		return err
+	}
+	return km.FromStringMap(tmp)
+}
+
+// MarshalYAML writes yaml configuration
+func (km KeyMap) MarshalYAML() (interface{}, error) {
+	return km.StringMap(), nil
+}
+
+// KeyActionsMap is used to validate if a certain action exists
+var KeyActionsMap map[string]KeyActions = map[string]KeyActions{
+	KeyActionNil.String():       KeyActionNil,
+	KeyActionMap.String():       KeyActionMap,
+	KeyActionDown.String():      KeyActionDown,
+	KeyActionUp.String():        KeyActionUp,
+	KeyActionTap.String():       KeyActionTap,
+	KeyActionDoubleTap.String(): KeyActionDoubleTap,
+	KeyActionHold.String():      KeyActionHold,
+}
+
+// ParseKeyAction returns the KeyAction from a string, or an error if it is invalid
+func ParseKeyAction(value string) (KeyActions, error) {
+	if value == "" {
+		return KeyActionMap, nil
+	}
+	a, ok := KeyActionsMap[value]
+	if !ok {
+		return a, fmt.Errorf("Action %s not found", value)
+	}
+	return a, nil
+}
+
+// ParseKbAction returns the KbAction from a string, or an error if it is invalid
+func ParseKbAction(value string) (KbActions, error) {
+	if value == "" {
+		return KbActionMap, nil
+	}
+	a, ok := KbActionsMap[value]
+	if !ok {
+		return a, fmt.Errorf("Action %s not found", value)
+	}
+	return a, nil
+}
+
+// ParseKeyCode returns the KeyCode from a string, or an error if it is invalid
+func ParseKeyCode(value string) (KeyCode, error) {
+	if value == "" {
+		return KeyCode(0), nil
+	}
+	code, ok := ecodes[value]
+	if !ok {
+		return KeyCode(code), fmt.Errorf("Code %s not found", value)
+	}
+	return KeyCode(code), nil
+}
+
+// UnmarshalYAML reads yaml configuration
+func (ev *KbActions) UnmarshalYAML(value *yaml.Node) error {
+	var actionString string
+	err := value.Decode(&actionString)
+	if err != nil {
+		return err
+	}
+	action, err := ParseKbAction(actionString)
+	if err != nil {
+		return err
+	}
+	*ev = action
+	return nil
+}
+
+// UnmarshalYAML reads yaml configuration
+func (ev *KeyActions) UnmarshalYAML(value *yaml.Node) error {
+	var actionString string
+	err := value.Decode(&actionString)
+	if err != nil {
+		return err
+	}
+	action, err := ParseKeyAction(actionString)
+	if err != nil {
+		return err
+	}
+	*ev = action
+	return nil
+}
+
+// MarshalYAML writes yaml configuration
+func (action KeyActions) MarshalYAML() (interface{}, error) {
+	return action.String(), nil
+}
+
+// MarshalYAML writes yaml configuration
+func (action KbActions) MarshalYAML() (interface{}, error) {
+	return action.String(), nil
+}
+
+// UnmarshalYAML reads yaml configuration
+func (ev *KeyCode) UnmarshalYAML(value *yaml.Node) error {
+	var codeString string
+	err := value.Decode(&codeString)
+	if err != nil {
+		return err
+	}
+	code, err := ParseKeyCode(codeString)
+	if err != nil {
+		return err
+	}
+	*ev = code
+	return nil
+}
+
+// MarshalYAML writes yaml configuration
+func (keyCode KeyCode) MarshalYAML() (interface{}, error) {
+	return keyCode.String(), nil
+}
---
+++ pkg/chkb/config_test.go
@@ -0,0 +1,152 @@
+/*
+Copyright © 2020 Víctor Pérez @MetalBlueberry
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package chkb
+
+import (
+	"bytes"
+	"strings"
+
+	evdev "github.com/gvalkov/golang-evdev"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
+)
+
+var _ = Describe("Config", func() {
+
+	Describe("Load", func() {
+		var (
+			fileContent = `layers:
+    base:
+        keyMap:
+            KEY_LEFTSHIFT:
+                Tap:
+                  - action: PushLayer
+                    layerName: swapAB
+    swapAB:
+        keyMap:
+            KEY_A:
+                Map:
+                  - keyCode: KEY_B
+            KEY_B:
+                Map:
+                  - keyCode: KEY_A
+            KEY_LEFTSHIFT:
+                Tap:
+                  - action: PopLayer
+`
+		)
+		It("Load simple", func() {
+			book := Config{}
+			err := book.Load(strings.NewReader(fileContent))
+			Expect(err).To(BeNil())
+			Expect(book.Layers).To(HaveKey("base"))
+			Expect(book.Layers).To(HaveKey("swapAB"))
+
+			Expect(book.Layers["base"].KeyMap).To(HaveKey(KeyCode(evdev.KEY_LEFTSHIFT)))
+			Expect(book.Layers["base"].KeyMap[KeyCode(evdev.KEY_LEFTSHIFT)]).
+				To(HaveKeyWithValue(KeyActionTap, []MapEvent{
+					{
+						Action:    KbActionPushLayer,
+						LayerName: "swapAB",
+					},
+				}))
+			Expect(book.Layers["swapAB"].KeyMap).To(HaveKey(KeyCode(evdev.KEY_A)))
+			Expect(book.Layers["swapAB"].KeyMap[KeyCode(evdev.KEY_A)]).
+				To(HaveKeyWithValue(KeyActionMap, []MapEvent{
+					{
+						KeyCode: evdev.KEY_B,
+					},
+				}))
+		})
+		It("Save simple", func() {
+			book := Config{
+				Layers: LayerBook{
+					"base": {
+						KeyMap: map[KeyCode]KeyMapActions{
+							evdev.KEY_LEFTSHIFT: {KeyActionTap: {{Action: KbActionPushLayer, LayerName: "swapAB"}}},
+						},
+					},
+					"swapAB": {
+						KeyMap: map[KeyCode]KeyMapActions{
+							evdev.KEY_LEFTSHIFT: {KeyActionTap: {{Action: KbActionPopLayer}}},
+							evdev.KEY_A:         {KeyActionMap: {{KeyCode: evdev.KEY_B}}},
+							evdev.KEY_B:         {KeyActionMap: {{KeyCode: evdev.KEY_A}}},
+						},
+					},
+				},
+			}
+			buf := &bytes.Buffer{}
+			book.Save(buf)
+
+			Expect(buf.String()).To(Equal(fileContent))
+		})
+	})
+
+	DescribeTable("findMap", func(layer *Layer, event KeyEvent, expected []MapEvent, foundExpected bool) {
+		obtained, found := layer.findMap(event)
+		assert.Equal(GinkgoT(), foundExpected, found)
+		assert.Equal(GinkgoT(), expected, obtained)
+	},
+		Entry("Not found",
+			&Layer{
+				KeyMap: KeyMap{KeyCode(KEY_A): {KeyActionMap: {{KeyCode: KEY_B}}}},
+			},
+			KeyEvent{KeyCode: KEY_B, Action: KeyActionDown},
+			nil,
+			false,
+		),
+		Entry("MapA keyDown",
+			&Layer{
+				KeyMap: KeyMap{KeyCode(KEY_A): {KeyActionMap: {{KeyCode: KEY_B}}}},
+			},
+			KeyEvent{KeyCode: KEY_A, Action: KeyActionDown},
+			[]MapEvent{{KeyCode: KEY_B, Action: KbActionDown}},
+			true,
+		),
+		Entry("MapA keyUp",
+			&Layer{
+				KeyMap: KeyMap{KeyCode(KEY_A): {KeyActionMap: {{KeyCode: KEY_B}}}},
+			},
+			KeyEvent{KeyCode: KEY_A, Action: KeyActionUp},
+			[]MapEvent{{KeyCode: KEY_B, Action: KbActionUp}},
+			true,
+		),
+		Entry("ActionDown",
+			&Layer{
+				KeyMap: KeyMap{KeyCode(KEY_A): {KeyActionDown: {{KeyCode: KEY_B}}}},
+			},
+			KeyEvent{KeyCode: KEY_A, Action: KeyActionDown},
+			[]MapEvent{{KeyCode: KEY_B, Action: KbActionDown}},
+			true,
+		),
+		Entry("ActionUp",
+			&Layer{
+				KeyMap: KeyMap{KeyCode(KEY_A): {KeyActionUp: {{KeyCode: KEY_B}}}},
+			},
+			KeyEvent{KeyCode: KEY_A, Action: KeyActionUp},
+			[]MapEvent{{KeyCode: KEY_B, Action: KbActionUp}},
+			true,
+		),
+	)
+})
