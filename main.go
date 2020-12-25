86fe3cb1df1c112068ffdc7cdc33b375743a0400
Metalblueberry <metalblueberry@gmail.com>
2020-12-25 12:34:54 +0100

    change main description

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+
+// chkb turns a regular keyboard intro a fully programmable keyboard.
 package main
 
 import "github.com/MetalBlueberry/chkb/cmd"
